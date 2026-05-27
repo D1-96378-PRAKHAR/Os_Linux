fe8b2717404b628d31cc0ad02aed133a869a98fb
PrakharVishwakarma <prakharvishwapkv180@gmail.com>
2026-05-28 00:12:12 +0530

    SDM_Assignment_Day07 : Q1 Done

---
+++ SDM-Assignment-Day07-Q1/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "WHO COMMAND : "
+who
+
+echo ""
+
+echo "PRESENT WORKING DIRECTORY : "
+pwd
+
+echo ""
+
+echo "CURRENT DATE : "
+date
+
+echo ""
+
+echo "USER ID INFORMATION : "
+id
--- SDM-Assignment1/server.js
+++ SDM-Assignment1/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express(); //
 const port = 3000; //
 
+const os = require("os");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
